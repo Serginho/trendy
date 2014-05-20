3a880e91f5071e62762def67196aa81671b10400
Serginho <madridserginho@hotmail.com>
2014-05-20 23:08:30 +0200

    Fron-end design has been integrated

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Trendy::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  resources :posts
+
+  root 'posts#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
